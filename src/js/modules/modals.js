bbc4fb4d6c0b2ccc0ce81fde59479f97eea47847
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-07 23:18:36 +0200

    refactor modal functionality to use class for body overflow management

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,20 +4,24 @@ const modals = () => {
             if (e.target) {
                 e.preventDefault();
             }
-            modalSelector.style.display = 'block' ;
-            document.body.style.overflow = 'hidden';
+            modalSelector.style.display = 'block';
+
+            document.body.classList.add('modal-open')
         })
         closeSelector.addEventListener('click', () => {
             modalSelector.style.display = 'none';
-            document.body.style.overflow = '';
+
+            document.body.classList.remove('modal-open')
         })
         modalSelector.addEventListener('click', (e) => {
             if (e.target === modalSelector) {
                 modalSelector.style.display = 'none';
-                document.body.style.overflow = '';
+
+                document.body.classList.remove('modal-open')
             }
         })
     }
+
     const callEngineerButton = document.querySelector('.popup_engineer_btn'),
         modalEngineer = document.querySelector('.popup_engineer'),
         modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
