403ba71290622d6320b60d825bb319a9c43f2bf9
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-10 01:53:38 +0200

    add modalState initialization and integrate changeModalState function

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,17 @@ import "./slider";
 import modals from "./modules/modals.js";
 import tabs from "./modules/tabs.js";
 import forms from "./modules/forms.js";
+import changeModalState from "./modules/changeModalState";
 
 window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    let modalState = {
+
+    };
+
+    changeModalState(modalState);
+
     modals();
     tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
