835f23ca8b6bb7f7f06e0935677e4c6d678f9046
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-10 01:26:51 +0200

    add closeClickOverlay parameter to bindModal function for enhanced modal behavior

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,9 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach((item) => {
             item.addEventListener('click', (e) => {
@@ -10,6 +11,9 @@ const modals = () => {
                     e.preventDefault();
                 }
                 const scrollWidth = getScrollWidth()
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
 
                 modal.style.display = 'block';
                 document.body.classList.add('modal-open')
@@ -21,13 +25,20 @@ const modals = () => {
         close.addEventListener('click', () => {
             modal.style.display = 'none';
 
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
             document.body.classList.remove('modal-open')
             document.body.style.marginRight = '';
         })
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
                 modal.style.display = 'none';
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
 
                 document.body.classList.remove('modal-open')
                 document.body.style.marginRight = '';
@@ -41,14 +52,18 @@ const modals = () => {
             document.body.classList.add('modal-open')
         }, time);
     }
+
     function getScrollWidth() {
         return window.innerWidth - document.documentElement.clientWidth;
     }
 
 
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close' );
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    bindModal('.popup_calc_btn','.popup_calc','.popup_calc_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+
     showModalByTime('.popup', 60000);
 
 
