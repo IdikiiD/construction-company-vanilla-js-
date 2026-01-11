5191dd752dfc3d5a0d96c15dbe225e75f724efa8
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-12 01:09:14 +0200

    add timer and image modules to main.js; integrate with modalState and forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
 import "./slider";
-import modals from "./modules/modals.js";
-import tabs from "./modules/tabs.js";
-import forms from "./modules/forms.js";
-import changeModalState from "./modules/changeModalState";
+import modals from './modules/modals';
+import tabs from './modules/tabs';
+import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
+import timer from "./modules/timer";
+import images from "./modules/images";
 
 window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+    "use strict";
 
-    let modalState = {
-
-    };
+    let modalState = {};
+    let deadline = '2026-02-31';
 
     changeModalState(modalState);
-
     modals();
     tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms();
-})
-
-
+    forms(modalState);
+    timer('.container1', deadline);
+    images()
+});
