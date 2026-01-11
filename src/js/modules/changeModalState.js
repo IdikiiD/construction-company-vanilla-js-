266413bae85559138512b96e9428781b44f28b41
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-12 01:08:51 +0200

    refactor changeModalState.js for consistency and readability

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,7 +1,7 @@
-import checkNumInputs from "./checkNumInputs";
+import checkNumInputs from './checkNumInputs';
 
 const changeModalState = (state) => {
-    const windowForms = document.querySelectorAll('.balcon_icons_img'),
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
         windowWidth = document.querySelectorAll('#width'),
         windowHeight = document.querySelectorAll('#height'),
         windowType = document.querySelectorAll('#view_type'),
@@ -10,41 +10,41 @@ const changeModalState = (state) => {
     checkNumInputs('#width');
     checkNumInputs('#height');
 
-    function bindActionToElems(event, elem, prop) {
+    function bindActionToElems (event, elem, prop) {
         elem.forEach((item, i) => {
             item.addEventListener(event, () => {
-                switch (item.nodeName) {
-                    case 'SPAN':
+                switch(item.nodeName) {
+                    case 'SPAN' :
                         state[prop] = i;
                         break;
-                    case 'INPUT':
+                    case 'INPUT' :
                         if (item.getAttribute('type') === 'checkbox') {
                             i === 0 ? state[prop] = "Холодное" : state[prop] = "Теплое";
                             elem.forEach((box, j) => {
-                                box.checked = i === j;
+                                box.checked = false;
+                                if (i == j) {
+                                    box.checked = true;
+                                }
                             });
                         } else {
                             state[prop] = item.value;
                         }
                         break;
-                    case 'SELECT':
+                    case 'SELECT' :
                         state[prop] = item.value;
                         break;
                 }
+
                 console.log(state);
-            })
+            });
         });
     }
 
-
-
-    bindActionToElems('click', windowForms, 'form');
+    bindActionToElems('click', windowForm, 'form');
     bindActionToElems('input', windowHeight, 'height');
     bindActionToElems('input', windowWidth, 'width');
     bindActionToElems('change', windowType, 'type');
     bindActionToElems('change', windowProfile, 'profile');
+};
 
-
-}
-
-export default changeModalState
+export default changeModalState;
